258f80e22d891d54ab4cf390d9fbfac74131b798
RafaCappetta <rafaelchiappettabarboza@gmail.com>
2025-05-17 14:53:05 -0300

    Adicionado arquivos com código e TXT de filtragem
     O arquivo txt está em branco inicialmente e será aberto em formato "w".

---
+++ Filtragem.py
@@ -0,0 +1,22 @@
+def filtrar(movtreino):
+    existe_no_crud = 0
+    crud = open("arquivo crud.txt", "r", encoding='utf8')
+    linhascrud = crud.readlines()
+    crud.close()
+    filtrando = open("Filtragem.txt", "w", encoding='utf8')
+
+    for i in range(len(linhascrud)):
+        if movtreino in linhascrud[i]:
+            filtrando.write(linhascrud[i] + "\n")
+            existe_no_crud += 1
+
+    filtrando.close()
+    
+    if existe_no_crud == 0:
+        return print("Treino ou movimento não existe no crud.")
+    
+    else:
+        return print("Filtragem concluída")
+
+Wodmov_filt = input("Insira o treino ou movimento que deseja filtrar: ").upper()
+filtrar(Wodmov_filt)
